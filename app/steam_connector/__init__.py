c62d2d85aa9f4f6940d9189ef50269f9a11fc2cc
Parker Scott <98108108+Snazzy11@users.noreply.github.com>
2025-07-14 18:44:49 -0400

    Better error handling and resiliency

--- app/steam_connector/__init__.py
+++ app/steam_connector/__init__.py
@@ -1,12 +1,10 @@
 """
-Provides the connection to the Steam API. Also provides functions to fetch from the Steam API
+Provides the connection to the Steam API
 """
-
+# TODO: Use an interface or something so that when responses fail there is a fallback
 import os
 import json
-
 from dotenv import load_dotenv
-
 import requests
 
 load_dotenv()
@@ -17,18 +15,25 @@ class ApiItem:
     """
     A method provider for returned JSON data.
     """
-    data = object
-    str_raw_json = ''
-    pretty_json = ''
+    def __init__(self, json_data_string='', error=None):
+        self.str_raw_json = json_data_string or ''
+        self.error = error
+        self.success = error is None and json_data_string != ''
+        self.data = None
+        self.pretty_json = ''
 
-    def __init__(self, json_data_string):
-        self.str_raw_json = json_data_string
-        self.data = json.loads(json_data_string)
-        self.pretty_json = json.dumps(self.data, indent=2)
+        if self.success:
+            try:
+                self.data = json.loads(json_data_string)
+                self.pretty_json = json.dumps(self.data, indent=2)
+            except json.JSONDecodeError as e:
+                self.success = False
+                self.error = f"JSON decode error: {e}"
+                self.data = None
+
 
     def __str__(self):
-        return self.str_raw_json
-
+        return self.pretty_json if self.success else f"<Error: {self.error}>"
 
 class SteamApi:
     """
@@ -36,23 +41,49 @@ class SteamApi:
     This class is NOT for processing data returned from the Steam API
     The functions will return Python objects which will have their own data and methods to process them
     """
+    def __init__(self, api_key: str):
+        self.api_key = api_key
 
-    url_GetOwnedGames = ('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + steam_api_key +
-                         '&steamid=' + '%PLAYERID%' + '&format=json')
-
-    def player_get_owned_games(self, player_id: str) -> json:
+    def get_owned_games(self, player_id: str) -> ApiItem:
         """
         Queries the Steam API for games owned by a player
-        :return: Full json text response from the Steam API
+        :method: IPlayerService
+        :param player_id: Numerical player ID
+        :return: ApiItem object
         """
+        url = (f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v1/'
+               f'?key={self.api_key}&steamid={player_id}&format=json')
+        try:
+            response = requests.get(url, timeout=5)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            return ApiItem(error=str(e))
+        return ApiItem(response.text)
 
-        url = self.url_GetOwnedGames.replace('%PLAYERID%', player_id)
-
-        response = requests.get(url, timeout=5)
+    def get_game_stats(self, player_id: str, game_id: str,
+                       date_range_start='2000-01-01', date_range_end='2030-01-01') -> ApiItem:
+        """
+        Queries the Steam API for game statistics for a plyaer on a specific game
+        :method: ISteamGameServerStats
+        :param player_id: Numerical player ID
+        :param game_id: Numerical game or APP ID (has specific logic but should usually be unnecessary)
+        :param date_range_start: Format YYYY-MM-DD HH:MM:SS the starting date to query
+        :param date_range_end: Format YYYY-MM-DD HH:MM:SS the ending date to query
+        :return: ApiItem object
+        """
+        url = (f'http://partner.steam-api.com/ISteamGameServerStats/GetGameServerPlayerStatsForGame/v1/'
+               f'?key={player_id}&gameid={game_id}&appid={game_id}'
+               f'&rangestart={date_range_start}&rangeend={date_range_end}')
+        try:
+            response = requests.get(url, timeout=5)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            return ApiItem(error=str(e))
         return ApiItem(response.text)
 
 
 
-SteamApi = SteamApi()
+SteamApi = SteamApi(os.getenv('STEAM_API_KEY'))
 
+print(SteamApi.get_game_stats(os.getenv('MY_STEAM_ID'), '1172470'))
 # print(SteamApi.player_get_owned_games(os.getenv('MY_STEAM_ID')).pretty_json)
